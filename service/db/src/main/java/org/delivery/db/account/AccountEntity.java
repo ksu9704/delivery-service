5d4ca7b6a5789bcd677c8b79c5e7c07be26dfae0
Kong Sang Uk <qkqhfp19@naver.com>
2023-07-15 16:47:26 +0900

    수정

--- service/db/src/main/java/org/delivery/db/account/AccountEntity.java
+++ service/db/src/main/java/org/delivery/db/account/AccountEntity.java
@@ -1,10 +1,7 @@
 package org.delivery.db.account;
 
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.delivery.db.BaseEntity;
 
