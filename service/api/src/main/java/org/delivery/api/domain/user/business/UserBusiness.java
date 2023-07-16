c20eee82bbf0aafdcc5e9bdcc25cf568c81b2488
Kong Sang Uk <qkqhfp19@naver.com>
2023-07-17 01:46:24 +0900

    user domain

---
+++ service/api/src/main/java/org/delivery/api/domain/user/business/UserBusiness.java
@@ -0,0 +1,63 @@
+package org.delivery.api.domain.user.business;
+
+import lombok.RequiredArgsConstructor;
+import org.delivery.api.common.annotation.Business;
+import org.delivery.api.domain.user.controller.model.UserLoginRequest;
+import org.delivery.api.domain.user.controller.model.UserRegisterRequest;
+import org.delivery.api.domain.user.controller.model.UserResponse;
+import org.delivery.api.domain.user.model.User;
+import org.delivery.api.domain.user.service.UserService;
+import org.delivery.api.domain.token.business.TokenBusiness;
+import org.delivery.api.domain.token.controller.model.TokenResponse;
+import org.delivery.api.domain.user.converter.UserConverter;
+
+@RequiredArgsConstructor
+@Business
+public class UserBusiness {
+
+    private final UserService userService;
+    private final UserConverter userConverter;
+
+    private final TokenBusiness tokenBusiness;
+
+    /**
+     * 사용자에 대한 가입처리 로직
+     * 1. request -> entity
+     * 2. entity -> save
+     * 3. save Entity -> response
+     * 4. response return
+     */
+    public UserResponse register(UserRegisterRequest request) {
+
+        var entity = userConverter.toEntity(request);
+        var newEntity = userService.register(entity);
+        var response = userConverter.toResponse(newEntity);
+        return response;
+
+        /*return Optional.ofNullable(request)
+            .map(userConverter::toEntity)
+            .map(userService::register)
+            .map(userConverter::toResponse)
+            .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT, "request null"));*/
+    }
+
+    /**
+     * 1. email, password 를 가지고 사용자 체크
+     * 2. user entity 로그인 확인
+     * 3. token 생성
+     * 4. token response
+     */
+    public TokenResponse login(UserLoginRequest request) {
+        var userEntity = userService.login(request.getEmail(), request.getPassword());
+        var tokenResponse = tokenBusiness.issueToken(userEntity);
+        return tokenResponse;
+    }
+
+    public UserResponse me(
+        User user
+    ) {
+        var userEntity = userService.getUserWithThrow(user.getId());
+        var response = userConverter.toResponse(userEntity);
+        return response;
+    }
+}
---
+++ service/api/src/main/java/org/delivery/api/domain/user/controller/UserApiController.java
@@ -0,0 +1,27 @@
+package org.delivery.api.domain.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.delivery.api.common.annotation.UserSession;
+import org.delivery.api.common.api.Api;
+import org.delivery.api.domain.user.model.User;
+import org.delivery.api.domain.user.business.UserBusiness;
+import org.delivery.api.domain.user.controller.model.UserResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/user")
+public class UserApiController {
+
+    private final UserBusiness userBusiness;
+
+    @GetMapping("/me")
+    public Api<UserResponse> me(
+        @UserSession User user
+    ){
+        var response = userBusiness.me(user);
+        return Api.OK(response);
+    }
+}
