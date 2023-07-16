675c409f156931a38912705c7ef726818cb96574
Kong Sang Uk <qkqhfp19@naver.com>
2023-07-17 01:45:57 +0900

    UserSessionResolver

--- service/api/src/main/java/org/delivery/api/config/web/WebConfig.java
+++ service/api/src/main/java/org/delivery/api/config/web/WebConfig.java
@@ -2,7 +2,9 @@ package org.delivery.api.config.web;
 
 import lombok.RequiredArgsConstructor;
 import org.delivery.api.interceptor.AuthorizationInterceptor;
+import org.delivery.api.resolver.UserSessionResolver;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,6 +15,7 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
 
     private final AuthorizationInterceptor authorizationInterceptor;
+    private final UserSessionResolver userSessionResolver;
 
     private List<String> OPEN_API = List.of(
             "/open-api/**"
@@ -39,4 +42,10 @@ public class WebConfig implements WebMvcConfigurer {
         ;
 
     }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(userSessionResolver);
+    }
+
 }
