27df26445221ce5b9e887297b47c8dec98df99fa
Kong Sang Uk <qkqhfp19@naver.com>
2023-07-16 19:42:55 +0900

    AccountApiController

--- service/api/src/main/java/org/delivery/api/account/AccountApiController.java
+++ service/api/src/main/java/org/delivery/api/account/AccountApiController.java
@@ -2,6 +2,10 @@ package org.delivery.api.account;
 
 import lombok.RequiredArgsConstructor;
 import org.delivery.api.account.model.AccountMeResponse;
+import org.delivery.api.common.api.Api;
+import org.delivery.api.common.error.ErrorCode;
+import org.delivery.api.common.error.UserErrorCode;
+import org.delivery.api.common.exception.ApiException;
 import org.delivery.db.account.AccountEntity;
 import org.delivery.db.account.AccountRepository;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,19 +21,25 @@ public class AccountApiController {
 
     private final AccountRepository accountRepository;
 
-//    @GetMapping("")
-//    public void save(){
-//        var account = AccountEntity.builder().build();
-//        accountRepository.save(account);
-//    }
-
     @GetMapping("/me")
-    public AccountMeResponse me(){
+    public Api<AccountMeResponse> me(){
 
-        return AccountMeResponse.builder()
-            .name("홍길동")
-            .email("A@gmail.com")
-            .registeredAt(LocalDateTime.now())
-            .build();
+        var response = AccountMeResponse.builder()
+                .name("홍길동")
+                .email("A@gmail.com")
+                .registeredAt(LocalDateTime.now())
+                .build();
+
+        var str = "안녕하세요";
+        var age = 0;
+
+        try{
+            Integer.parseInt(str);
+
+        }catch (Exception e){
+            throw new ApiException(ErrorCode.SERVER_ERROR, e , "사용자 Me 호출시 에러 발생");
+        }
+
+        return Api.OK(response);
     }
 }
