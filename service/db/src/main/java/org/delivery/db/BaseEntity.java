d4d39c6a4c908b8b50bf989bd6ecc40b60e3043a
Kong Sang Uk <qkqhfp19@naver.com>
2023-07-16 20:21:48 +0900

    UserEntity

--- service/db/src/main/java/org/delivery/db/BaseEntity.java
+++ service/db/src/main/java/org/delivery/db/BaseEntity.java
@@ -1,7 +1,9 @@
 package org.delivery.db;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.GeneratedValue;
@@ -11,6 +13,8 @@ import javax.persistence.MappedSuperclass;
 @MappedSuperclass
 @Data
 @SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BaseEntity {
 
     @Id
--- service/db/src/main/java/org/delivery/db/account/AccountRepository.java
+++
@@ -1,6 +0,0 @@
-package org.delivery.db.account;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<AccountEntity, Long> {
-}
