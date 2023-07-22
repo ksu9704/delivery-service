629918028e465f2abee8b51b8659242a2f0aef02
Kong Sang Uk <qkqhfp19@naver.com>
2023-07-22 22:22:02 +0900

    rabbitmq

--- service/api/src/main/java/org/delivery/api/domain/userorder/business/UserOrderBusiness.java
+++ service/api/src/main/java/org/delivery/api/domain/userorder/business/UserOrderBusiness.java
@@ -11,6 +11,7 @@ import org.delivery.api.domain.userorder.controller.model.UserOrderDetailRespons
 import org.delivery.api.domain.userorder.controller.model.UserOrderRequest;
 import org.delivery.api.domain.userorder.controller.model.UserOrderResponse;
 import org.delivery.api.domain.userorder.converter.UserOrderConverter;
+import org.delivery.api.domain.userorder.producer.UserOrderProducer;
 import org.delivery.api.domain.userorder.service.UserOrderService;
 import org.delivery.api.domain.userordermenu.converter.UserOrderMenuConverter;
 import org.delivery.api.domain.userordermenu.service.UserOrderMenuService;
@@ -36,6 +37,8 @@ public class UserOrderBusiness {
 
     private final StoreService storeService;
     private final StoreConverter storeConverter;
+
+    private final UserOrderProducer userOrderProducer;
 
 
     // 1. 사용자 , 메뉴 id
@@ -66,6 +69,10 @@ public class UserOrderBusiness {
         userOrderMenuEntityList.forEach(it ->{
             userOrderMenuService.order(it);
         });
+
+
+        // 비동기로 가맹점에 주문 알리기
+        userOrderProducer.sendOrder(newUserOrderEntity);
 
 
         // response
